9ed4bde443228bafc98053f4357c07f107055251
Jishuashi <chartierhugo098@gmail.com>
2021-03-02 13:52:07 +0100

    add Classes GameView + RadarView  and radarasset.swf

---
+++ game/src/com/isartdigital/nabokos/ui/GameView.hx
@@ -0,0 +1,18 @@
+package com.isartdigital.nabokos.ui;
+
+import com.isartdigital.utils.ui.Screen;
+
+/**
+ * ...
+ * @author Hugo CHARTIER
+ */
+class GameView extends Screen 
+{
+
+	public function new(?pLibrary:String="ui") 
+	{
+		super(pLibrary);
+		
+	}
+	
+}
---
+++ game/src/com/isartdigital/nabokos/ui/RadarView.hx
@@ -0,0 +1,16 @@
+package com.isartdigital.nabokos.ui;
+
+/**
+ * ...
+ * @author Hugo CHARTIER
+ */
+class RadarView extends GameView 
+{
+
+	public function new(?pLibrary:String="ui") 
+	{
+		super(pLibrary);
+		
+	}
+	
+}
