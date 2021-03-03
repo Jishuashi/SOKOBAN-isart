43e5a8e9d24fe4c57dfc2ad0fa3b42cd59e9b3ee
Anthony Tirel--Tartuffe <tony.tirell@gmail.com>
2021-03-03 19:00:17 +0100

    Adding consequences of player's action on currentLevel

--- game/src/com/isartdigital/nabokos/game/LevelManager.hx
+++ game/src/com/isartdigital/nabokos/game/LevelManager.hx
@@ -25,6 +25,8 @@ class LevelManager {
 	
 	private static var levels(default, null): Array<Array<Array<Blocks>>>;
 	
+	private static var playerPos: Point = new Point();
+	
 	private function new() {}
 	
 	/**
@@ -72,6 +74,8 @@ class LevelManager {
 			}
 		}
 		
+		selectLevel(0);
+		
 		for (i in 0...levels.length) {
 			trace(levels[i] + "\n");
 		}
@@ -85,8 +89,83 @@ class LevelManager {
 	public static function selectLevel(pLevel: Int): Bool{
 		if (pLevel < 0 || pLevel >= levels.length) return false;
 		
-		currentLevel = levels[pLevel];
+		currentLevel = levels[pLevel].copy();
+		
+		playerPos = new Point();
+		
+		for (y in 0...currentLevel.length) {
+			for (x in 0...currentLevel[y].length) {
+				if (currentLevel[y][x] == Blocks.PLAYER) {
+					currentLevel[y][x] = Blocks.GROUND;
+					
+					playerPos.x = x;
+					playerPos.y = y;
+					break;
+				}
+			}
+			
+			if (playerPos.x != 0 || playerPos.y != 0) break;
+		}
+		
 		return true;
+	}
+
+	/**
+	 * Simule les conséquences de l'action du joueur sur le plateau de jeu, puis effectue cette action si elle est valide
+	 * @param	pPlayerPosition
+	 * @param	pMove
+	 * @return	Bool indiquant si l'action que le joueur veut effectuer est faisable, ou non
+	 */
+	public static function playerAction(pMove: PlayerActions): Bool {
+		var lNextPosPlayer: Point = new Point(playerPos.x, playerPos.y);
+		
+		switch (pMove) {
+			case PlayerActions.LEFT:
+				lNextPosPlayer.x--;
+			
+			case PlayerActions.RIGHT:
+				lNextPosPlayer.x++;
+			
+			case PlayerActions.DOWN:
+				lNextPosPlayer.y++;
+			
+			case PlayerActions.UP:
+				lNextPosPlayer.y--;
+			
+			default:
+				return false;
+		}
+		
+		var lTargetTile: Blocks = currentLevel[Std.int(lNextPosPlayer.y)][Std.int(lNextPosPlayer.x)];
+		
+		if (lTargetTile == Blocks.GROUND || lTargetTile == Blocks.TARGET) {
+			playerPos = lNextPosPlayer;
+			
+			return true;
+		}
+		
+		if (lTargetTile == Blocks.BOX) {
+			var lNextPosBox: Point = new Point(lNextPosPlayer.x, lNextPosPlayer.y);
+			
+			lNextPosBox.x += lNextPosPlayer.x - playerPos.x;
+			lNextPosBox.y += lNextPosPlayer.y - playerPos.y;
+			
+			lTargetTile = currentLevel[Std.int(lNextPosPlayer.y)][Std.int(lNextPosPlayer.x)];
+			
+			if (lTargetTile == Blocks.GROUND || lTargetTile == Blocks.TARGET) {
+				currentLevel[Std.int(lNextPosBox.y)][Std.int(lNextPosBox.x)] = Blocks.BOX;
+				currentLevel[Std.int(lNextPosPlayer.y)][Std.int(lNextPosPlayer.x)] = levels[levels.indexOf(currentLevel)][Std.int(lNextPosPlayer.y)][Std.int(lNextPosPlayer.x)];
+				
+				playerPos.x = lNextPosPlayer.x;
+				playerPos.y = lNextPosPlayer.y;
+				
+				return true;
+			}
+		}
+		
+		if (lTargetTile == Blocks.WALL || lTargetTile == Blocks.MIRROR || lTargetTile == Blocks.BOX) return false;
+		
+		return false;
 	}
 
 }
